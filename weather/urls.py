2266d377c8b920e26b5086ebe6183671a7927cbc
Lazarus <76733643+NikitaKobzar345@users.noreply.github.com>
2021-08-10 00:13:00 +1000

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . import views
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('accounts/register/', views.registerPage, name='register'),
-    path('accounts/login/', views.auth, name='login'),
-    path('accounts/verify', views.verify, name='verify'),
-    path('accounts/logout/', views.logoutUser, name='logout'),
-    path('weather/', views.weather, name='weather'),
-    path('air-pollution/', views.air_pollution, name='air-pollution'),
-    path('contact/', views.contact, name='contact'),
-    path('success/', views.success, name='success'),
-    path('accounts/subscribe/', views.subscribe, name='subscribe')
+    path('', cache_page(60)(views.home), name='home'),
+    path('accounts/register/', cache_page(60)(views.registerPage), name='register'),
+    path('accounts/login/', cache_page(60)(views.auth), name='login'),
+    path('accounts/verify', cache_page(60)(views.verify), name='verify'),
+    path('accounts/logout/', cache_page(60)(views.logoutUser), name='logout'),
+    path('weather/', cache_page(60)(views.weather), name='weather'),
+    path('air-pollution/', cache_page(60)(views.air_pollution), name='air-pollution'),
+    path('contact/', cache_page(60)(views.contact), name='contact'),
+    path('success/', cache_page(60)(views.success), name='success'),
+    path('accounts/subscribe', cache_page(60)(views.subscribe), name='subscribe')
 ]
